9008f43e7ef0c76c238e8657cff03a658ec95c28
Ray Louie <raylouie@Raymonds-MacBook-Air-2.local>
2014-03-16 22:17:05 -0700

    first commit

--- UIScrollView Challenge Solution/CCViewController.m
+++ UIScrollView Challenge Solution/CCViewController.m
@@ -18,12 +18,23 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    self.globalImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"world-map.jpeg"]];
+    self.scrollView.contentSize = self.globalImageView.frame.size;
+    [self.scrollView addSubview:self.globalImageView];
+    self.scrollView.maximumZoomScale = 2.0;
+    self.scrollView.minimumZoomScale = 0.5;
+    self.scrollView.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    return self.globalImageView;
 }
 
 @end
